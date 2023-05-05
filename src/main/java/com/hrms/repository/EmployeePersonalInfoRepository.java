f23121500b0da9f0000c2e0500547f40ec8013e5
Tirumala <thotatirumala143@gmail.com>
2023-05-05 19:25:25 +0530

    tiru changes

--- src/main/java/com/hrms/repository/EmployeePersonalInfoRepository.java
+++ src/main/java/com/hrms/repository/EmployeePersonalInfoRepository.java
@@ -1,10 +1,13 @@
 package com.hrms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.hrms.entity.EmployeeJobHistory;
 
 public interface EmployeePersonalInfoRepository extends JpaRepository<EmployeeJobHistory, Integer> {
-
+	
+//    @Query("select positionId from EmployeeJobHistory where positionId=?1")
 	public EmployeeJobHistory getByPositionId(int positionId);
 	
 	public EmployeeJobHistory findByPositionId(int id);
