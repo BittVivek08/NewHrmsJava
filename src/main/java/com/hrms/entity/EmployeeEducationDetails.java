b9460ed91d27fc357da306e57cb1c9edd3f88a34
BittVivek08 <vivekkimar706277@gmail.com>
2023-06-12 18:48:29 +0530

    employee details changes

--- src/main/java/com/hrms/entity/EmployeeEducationDetails.java
+++ src/main/java/com/hrms/entity/EmployeeEducationDetails.java
@@ -67,9 +67,6 @@ public class EmployeeEducationDetails implements Serializable {
 	@Column(name = "otherDegreeName")
 	private String otherDegreeInstituteName;
 	
-	@Column(name = "user_id")
-	private Integer userId;
-
 	@Column(name = "educationlevel")
 	private int educationLevel;
 
