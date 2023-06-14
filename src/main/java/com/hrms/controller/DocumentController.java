a52b1e41fb7f93d09deef8fa458a5ff513049b40
Tirumala <thotatirumala143@gmail.com>
2023-06-14 19:12:05 +0530

    document changes

--- src/main/java/com/hrms/controller/DocumentController.java
+++ src/main/java/com/hrms/controller/DocumentController.java
@@ -2,15 +2,16 @@ package com.hrms.controller;
 
 import java.io.IOException;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,69 +27,54 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/file")
 public class DocumentController {
-	
+
 	@Autowired
 	private EmployeeDocumentServiceImpl empDocServ;
-	
-	@PostMapping("/uploadDocuments/{empId}")
-	public SuccessResponseBean uploadDocuments(@RequestParam("file") MultipartFile file,@RequestBody EmployeeDocumentsEntity entity,@PathVariable("empId") String empId) throws IOException {
-		log.info("upload documents api");
-		return empDocServ.uploadDocuments(file,entity,empId);
-	}
-	
-	@GetMapping("/{fileName}")
-	public ResponseEntity<?> downloadImage(@PathVariable String fileName){
-		log.info("get the document api");
-		byte[] fileData=empDocServ.downloadFile(fileName);
-		
-        // Determine the media type based on the file extension
-        MediaType mediaType = empDocServ.getMediaTypeForFileName(fileName);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(mediaType);
+	@PostMapping("/uploadDocuments/{empId}")
+	public SuccessResponseBean uploadDocuments(@RequestParam("file") MultipartFile file,
+			@RequestBody EmployeeDocumentsEntity entity, @PathVariable("empId") String empId) throws IOException {
+		log.info("upload documents api");
+		return empDocServ.uploadDocuments(file, entity, empId);
+	}
+
+	@GetMapping("/{fileName}")
+	public ResponseEntity<?> downloadImage(@PathVariable String fileName) {
+		log.info("get the document api");
+		byte[] fileData = empDocServ.downloadFile(fileName);
+
+		// Determine the media type based on the file extension
+		MediaType mediaType = empDocServ.getMediaTypeForFileName(fileName);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(mediaType);
 //        headers.set("Content-Type", "application/msword");
 //        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=document.doc");
-        headers.setContentDispositionFormData(fileName, fileName);
+		headers.setContentDispositionFormData(fileName, fileName);
 
-        ByteArrayResource resource = new ByteArrayResource(fileData);
+		ByteArrayResource resource = new ByteArrayResource(fileData);
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(resource);
+		return ResponseEntity.ok().headers(headers).body(resource);
 
 	}
 
-	 @GetMapping("/getallfiles/{empId}")
-	    public List<EmployeeDocumentResponse> getFilesByEmpId(@PathVariable String empId) {
-		 
-		 
-//	        List<byte[]> fileDataList = empDocServ.getFilesByEmpId(empId);
-	        
-//	        // Find the file data matching the provided fileName
-//	        byte[] fileData = fileDataList.stream()
-//	                .filter(data -> empDocServ.getFileName(data).equals(fileName))
-//	                .findFirst()
-//	                .orElse(null);
-//
-//	        if (fileData == null) {
-//	            // Handle the case when the file is not found
-//	            return ResponseEntity.notFound().build();
-//	        }
-
-//	        MediaType mediaType = empDocServ.getMediaTypeForFileName(fileName);
-//
-//	        HttpHeaders headers = new HttpHeaders();
-//	        headers.setContentType(mediaType);
-//	        headers.setContentDispositionFormData(fileName, fileName);
-//
-//	        ByteArrayResource resource = new ByteArrayResource(fileData);
-//
-//	        return ResponseEntity.ok()
-//	                .headers(headers)
-//	                .body(resource);
-		 
-		 List<EmployeeDocumentResponse> fileNameByEmpId = empDocServ.getFileNameByEmpId(empId);
-	        return fileNameByEmpId;
-	    }
-
+	@GetMapping("/getallfiles/{empId}")
+	public List<EmployeeDocumentResponse> getFilesByEmpId(@PathVariable String empId) {
+		log.info("get all files by empId documents api");
+		List<EmployeeDocumentResponse> fileNameByEmpId = empDocServ.getFileNameByEmpId(empId);
+		return fileNameByEmpId;
+	}
+	
+	@PutMapping("/updateDocuments/{id}")
+	public SuccessResponseBean updateDocuments(@RequestParam("file") MultipartFile file,
+			@RequestBody EmployeeDocumentsEntity entity, @PathVariable("id") int id) throws IOException {
+				
+		return empDocServ.updateDocument(file, entity, id);
+	}
+	
+	@DeleteMapping("/employee/{empId}/documents/{fileName}")
+    public SuccessResponseBean deleteEmployeeDocument(@PathVariable String empId, @PathVariable String fileName) {
+        return empDocServ.deleteFileNameByEmpId(empId, fileName);
+    }
 }
+
--- src/main/java/com/hrms/serviceImpl/EmployeeDocumentServiceImpl.java
+++ src/main/java/com/hrms/serviceImpl/EmployeeDocumentServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.apache.commons.compress.compressors.FileNameUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -33,15 +32,16 @@ public class EmployeeDocumentServiceImpl implements EmployeeDocumentService {
 	@Autowired
 	private SuccessResponseBean response;
 
-	public SuccessResponseBean uploadDocuments( MultipartFile file, EmployeeDocumentsEntity docsEntity,String empId) throws IOException {
+	@Override
+	public SuccessResponseBean uploadDocuments(MultipartFile file, EmployeeDocumentsEntity docsEntity, String empId)
+			throws IOException {
 		log.info("upload document bussiness logic method");
 		EmployeeDetails employee = this.employeeRepository.findByEmpId(empId);
 		List<EmployeeDetails> employeeDetails = this.employeeRepository.findByUserId(docsEntity.getUserId());
 
-		if(employee != null && employeeDetails != null && employeeDetails.size() > 0) {
-			EmployeeDocumentsEntity docEntity =empDocRepo.save(EmployeeDocumentsEntity.builder()
-					.documentName(file.getOriginalFilename())
-					.data(FileUtils.compressImage(file.getBytes())).build());
+		if (employee != null && employeeDetails != null && employeeDetails.size() > 0) {
+			EmployeeDocumentsEntity docEntity = empDocRepo.save(EmployeeDocumentsEntity.builder()
+					.documentName(file.getOriginalFilename()).data(FileUtils.compressImage(file.getBytes())).build());
 			docEntity.setEmpdetails(employee);
 			docEntity.setUserId(docsEntity.getUserId());
 			docEntity.setIsactive(docsEntity.getIsactive());
@@ -61,36 +61,36 @@ public class EmployeeDocumentServiceImpl implements EmployeeDocumentService {
 				response.setMessage("uploaded successfully....");
 				response.setStatus(true);
 				return response;
-			}
-			else {
+			} else {
 				response.setMessage("something is wrong....");
 				response.setStatus(false);
 				return response;
 			}
-		}
-		else {
+		} else {
 			response.setMessage("something is wrong....");
 			response.setStatus(false);
 			return response;
 		}
 	}
 
+	@Override
 	public byte[] downloadFile(String fileName) {
 		log.info("get the document bussiness logic method");
 		Optional<EmployeeDocumentsEntity> dbData = empDocRepo.findByDocumentFileName(fileName);
 		byte[] images = FileUtils.decompressImage(dbData.get().getData());
 		return images;
 	}
-	
-	public boolean isFileNameMatch(String data, String fileName) {
-        return data.equals(fileName);
-    }
 
+	public boolean isFileNameMatch(String data, String fileName) {
+		return data.equals(fileName);
+	}
+
+	@Override
 	public MediaType getMediaTypeForFileName(String fileName) {
 		log.info("get the mediaType checking bussiness logic method");
 		String[] arr = fileName.split("\\.");
 		String fileExtension = arr[arr.length - 1];
-		 
+
 		switch (fileExtension.toLowerCase()) {
 		case "png":
 			return MediaType.IMAGE_PNG;
@@ -107,38 +107,91 @@ public class EmployeeDocumentServiceImpl implements EmployeeDocumentService {
 			return MediaType.APPLICATION_OCTET_STREAM;
 		}
 	}
-	
-//	public List<byte[]> getFilesByEmpId(String empId){
-//		List<EmployeeDocumentsEntity> empDoc= empDocRepo.findByEmpId(empId);
-//		List<byte[]> fileDataList = new ArrayList<>();
-//
-//        for (EmployeeDocumentsEntity empDocs : empDoc) {
-//            fileDataList.add(empDocs.getData());
-//        }
-//
-//        return fileDataList;
-//	}
-	
-	public List<EmployeeDocumentResponse> getFileNameByEmpId(String empId){
-		
+
+	@Override
+	public List<EmployeeDocumentResponse> getFileNameByEmpId(String empId) {
+		log.info("get list of document filename by empId bussiness logic method");
 		List<Object[]> result = empDocRepo.findByEmpId(empId);
 		List<EmployeeDocumentResponse> response = new ArrayList<>();
-		for(int i=0; i< result.size(); i++) {
+		for (int i = 0; i < result.size(); i++) {
 			Object[] obj = result.get(i);
 			EmployeeDocumentResponse docResponse = new EmployeeDocumentResponse();
 			docResponse.setEmpId(obj[1].toString());
 			docResponse.setDocumentName(obj[0].toString());
-			
+
 			response.add(docResponse);
 		}
-		
+
+		return response;
+	}
+
+	@Override
+	public SuccessResponseBean updateDocument(MultipartFile file, EmployeeDocumentsEntity docsEntity, int id)
+			throws IOException {
+		log.info("update the document by empId bussiness logic method");
+//		EmployeeDetails employee = this.employeeRepository.findByEmpId(empId);
+		Optional<EmployeeDocumentsEntity> findById = empDocRepo.findById(id);
+//		List<EmployeeDetails> employeeDetails = this.employeeRepository.findByUserId(docsEntity.getUserId());
+
+		if (findById != null) {
+			EmployeeDocumentsEntity docEntity = empDocRepo.save(EmployeeDocumentsEntity.builder()
+					.documentName(file.getOriginalFilename()).data(FileUtils.compressImage(file.getBytes())).build());
+//			docEntity.setEmpdetails(employee);
+			docEntity.setUserId(docsEntity.getUserId());
+			docEntity.setIsactive(docsEntity.getIsactive());
+			docEntity.setCreatedBy(docsEntity.getCreatedBy());
+			docEntity.setDocumentId(docsEntity.getDocumentId());
+			docEntity.setIsExpires(docsEntity.getIsExpires());
+			docEntity.setDocumentFileName(docsEntity.getDocumentFileName());
+			docEntity.setModifiedBy(docsEntity.getModifiedBy());
+			docEntity.setCreatedBy(docsEntity.getCreatedBy());
+			docEntity.setCreatedDate(docsEntity.getCreatedDate());
+			docEntity.setFromDate(docsEntity.getFromDate());
+			docEntity.setToDate(docsEntity.getToDate());
+			docEntity.setModifiedDate(docsEntity.getModifiedDate());
+			this.empDocRepo.save(docEntity);
+
+			if (docEntity != null) {
+				response.setMessage("update document successfully....");
+				response.setStatus(true);
+				return response;
+			} else {
+				response.setMessage("something is wrong....");
+				response.setStatus(false);
+				return response;
+			}
+		} else {
+			response.setMessage("something is wrong....");
+			response.setStatus(false);
+			return response;
+		}
+	}
+
+	@Override
+	public SuccessResponseBean deleteFileNameByEmpId(String empId, String fileName) {
+
+		List<EmployeeDocumentsEntity> documents = empDocRepo.findByEmpIds(empId);
+
+		// Find the document with the given file name
+		Optional<EmployeeDocumentsEntity> documentOptional = documents.stream()
+				.filter(doc -> doc.getDocumentFileName().equals(fileName)).findFirst();
+
+		if (documentOptional.isPresent()) {
+			EmployeeDocumentsEntity documentToDelete = documentOptional.get();
+
+			// Delete the document
+			empDocRepo.delete(documentToDelete);
+
+			response.setMessage("File deleted successfully.");
+			response.setStatus(true);
+			return response;
+		}
+
+		response.setMessage("File not found.");
+		response.setStatus(false);
 		return response;
 	}
 }
-
-
-
-
 
 /////////////////////BackUp////////////////////////////////////////////////
 //public SuccessResponseBean uploadDocuments( MultipartFile file, EmployeeDocumentsEntity docsEntity,String eid) throws IOException {
@@ -170,8 +223,3 @@ public class EmployeeDocumentServiceImpl implements EmployeeDocumentService {
 //	}
 //	return response;
 //}
-
-
-
-
-
