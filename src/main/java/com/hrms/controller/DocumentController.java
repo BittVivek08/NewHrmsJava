08a7c669c08dfa8653f54a14be7697bb0cfd07a5
Tirumala <thotatirumala143@gmail.com>
2023-06-14 11:58:28 +0530

    document impl

--- src/main/java/com/hrms/controller/DocumentController.java
+++ src/main/java/com/hrms/controller/DocumentController.java
@@ -1,6 +1,8 @@
 package com.hrms.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.hrms.entity.EmployeeDocumentsEntity;
+import com.hrms.response.bean.EmployeeDocumentResponse;
 import com.hrms.response.bean.SuccessResponseBean;
 import com.hrms.serviceImpl.EmployeeDocumentServiceImpl;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -43,6 +46,8 @@ public class DocumentController {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(mediaType);
+//        headers.set("Content-Type", "application/msword");
+//        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=document.doc");
         headers.setContentDispositionFormData(fileName, fileName);
 
         ByteArrayResource resource = new ByteArrayResource(fileData);
@@ -53,5 +58,37 @@ public class DocumentController {
 
 	}
 
+	 @GetMapping("/getallfiles/{empId}")
+	    public List<EmployeeDocumentResponse> getFilesByEmpId(@PathVariable String empId) {
+		 
+		 
+//	        List<byte[]> fileDataList = empDocServ.getFilesByEmpId(empId);
+	        
+//	        // Find the file data matching the provided fileName
+//	        byte[] fileData = fileDataList.stream()
+//	                .filter(data -> empDocServ.getFileName(data).equals(fileName))
+//	                .findFirst()
+//	                .orElse(null);
+//
+//	        if (fileData == null) {
+//	            // Handle the case when the file is not found
+//	            return ResponseEntity.notFound().build();
+//	        }
+
+//	        MediaType mediaType = empDocServ.getMediaTypeForFileName(fileName);
+//
+//	        HttpHeaders headers = new HttpHeaders();
+//	        headers.setContentType(mediaType);
+//	        headers.setContentDispositionFormData(fileName, fileName);
+//
+//	        ByteArrayResource resource = new ByteArrayResource(fileData);
+//
+//	        return ResponseEntity.ok()
+//	                .headers(headers)
+//	                .body(resource);
+		 
+		 List<EmployeeDocumentResponse> fileNameByEmpId = empDocServ.getFileNameByEmpId(empId);
+	        return fileNameByEmpId;
+	    }
 
 }
--- src/main/java/com/hrms/repository/EmployeeDocumentsRepository.java
+++ src/main/java/com/hrms/repository/EmployeeDocumentsRepository.java
@@ -1,8 +1,10 @@
 package com.hrms.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hrms.entity.EmployeeDetails;
 import com.hrms.entity.EmployeeDocumentsEntity;
@@ -11,8 +13,13 @@ public interface EmployeeDocumentsRepository extends JpaRepository<EmployeeDocum
 
 	Optional<EmployeeDocumentsEntity> findByDocumentName(String fileName);
 	
+	Optional<EmployeeDocumentsEntity> findByDocumentFileName(String fileName);
+	
 	@Query("SELECT e1 FROM EmployeeDocumentsEntity e1 JOIN EmployeeDetails e2 ON e1.userId = e2.userId")
     public EmployeeDocumentsEntity findByUserIdInEmployeeDetails(Integer userId);
+	
+	@Query("SELECT e1.documentFileName, empdetails.empId FROM EmployeeDocumentsEntity e1 JOIN e1.empdetails empdetails WHERE empdetails.empId = :empId")
+	List<Object[]> findByEmpId(@Param("empId") String empId);
 
 //	@Query("UPDATE EmployeeDocumentsEntity doc SET doc.attachmentDocumentPath = :uploadPath WHERE doc.userId = :userId AND doc.documentId = :documentId")
 //	boolean uploadDocument(int userId, int documentId, String uploadPath);
